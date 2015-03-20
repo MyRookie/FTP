897dce4df85d88a7d524c2dca50e95d948c79995
rookie <rookie@userdeMacBook-Pro-2.local>
2015-03-20 22:43:41 +0200

    Initial Commit

---
+++ FTP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FTP
+//
+//  Created by rookie on 20/03/15.
+//  Copyright (c) 2015 rookie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
